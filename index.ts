9113c0448a4ab8c06b4bfb97ff112ddd4fcfc0fd
Grant Klimaytys <grantmk@users.noreply.github.com>
2023-10-13 16:16:21 +0100

    in

---
+++ index.ts
@@ -0,0 +1,11 @@
+import figlet from "figlet";
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    const body = figlet.textSync("I am Dev!");
+    return new Response(body);
+  },
+});
+
+console.log(`Bun version ${0}`, Bun.version)
