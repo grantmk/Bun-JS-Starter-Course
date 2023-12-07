3b162e112d4e3d2206c826b7d2ffc117b8e2831c
Grant Klimaytys <grantmk@users.noreply.github.com>
2023-12-07 09:55:32 +0000

    in

--- index.ts
+++ index.ts
@@ -1,13 +1,25 @@
 import figlet from "figlet";
+try {
+const serverDeets = Bun.file("serverdees.json")
+} catch (err) {
+  console.log(err)
+}
 
 const server = Bun.serve({
   port: 3000,
-  fetch(req) {
-    const body = figlet.textSync("I am Dev!");
-    return new Response(body);
+  async fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/") {
+      const body = figlet.textSync("I AM DEV");
+      return new Response(body)
+    } else if (url.pathname === "/version") {
+      return new Response((await serverDeets.json()).version)
+    } else {
+      return new Response(figlet.textSync("Not found haxx0r"), { status: 404 });
+    }
   },
 });
 
 console.log(`Bun version ${0}`, Bun.version)
-console.log(process.env.STARTUPMSG)
-console.log(process.env.SERVER)
+console.log(Bun.env.SERVERENDPOINT)
+console.log((await serverDeets.json()).version)
