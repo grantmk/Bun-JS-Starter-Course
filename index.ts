c8c66d09b3f30ad602a8effe0387e9b2eed96e8e
Grant Klimaytys <grantmk@users.noreply.github.com>
2024-02-20 14:37:43 +0000

    in

--- index.ts
+++ index.ts
@@ -1,30 +1,12 @@
 import figlet from "figlet";
 
-let serverDeets: any
-try {
-  //serverDeets = Bun.file("serverdeets.json")
-  if(serverDeets)
-    console.log((await serverDeets.json()).version)
-} catch (err) {
-  console.log(err)
-}
-
 const server = Bun.serve({
   port: 3000,
-  async fetch(req) {
-    const url = new URL(req.url);
-    if (url.pathname === "/") {
-      const body = figlet.textSync("I AM DEV");
-      return new Response(body)
-    } else if (url.pathname === "/version") {
-      if(serverDeets)
-        return new Response((await serverDeets.json()).version)
-      return new Response("Nothing.")
-    } else {
-      return new Response(figlet.textSync("Not found haxx0r"), { status: 404 });
-    }
+  fetch(req) {
+    const body = figlet.textSync("iAmDev")
+    return new Response(body);
   },
 });
 
+console.log(`Listening on http://localhost:${server.port} ...`);
 console.log(`Bun version ${0}`, Bun.version)
-console.log(Bun.env.SERVERENDPOINT)
