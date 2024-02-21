4a17bfa6bc004137cfaad242e1856ab1fffb14ee
Grant Klimaytys <grantmk@users.noreply.github.com>
2024-02-21 09:41:00 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,8 +3,25 @@ import figlet from "figlet";
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    const body = figlet.textSync("iAmDev")
-    return new Response(body);
+    const { method } = req
+    const { pathname } = new URL(req.url)
+
+    if (method === "POST" && pathname === "/posttest") {
+      return new Response(
+        JSON.stringify(
+          ["a", "list", "of", "items"]
+        ),
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+    } else if (pathname === "/") {
+      const body = figlet.textSync("I AM DEV");
+      return new Response(body)
+    } else if (pathname === "/version") {
+      return new Response(figlet.textSync("Details not available"))
+    } else {
+      return new Response(figlet.textSync("Not found haxx0r"), { status: 404 });
+    }
+
   },
 });
 
